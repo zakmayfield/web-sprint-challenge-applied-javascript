99b037fbd1b3e18bcafacccdd4a064558032b3dd
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-08-06 17:32:15 -0400

    images on the carousel aren't rendering?

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,36 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselCont = document.querySelector('.carousel-container')
+
+const CarouselMaker = () => {
+  const carousel = document.createElement('div')
+    const leftBtn = document.createElement('div')
+    const img1 = document.createElement('img')
+    const img2 = document.createElement('img')
+    const img3 = document.createElement('img')
+    const img4 = document.createElement('img')
+    const rightBtn = document.createElement('div')
+
+  carousel.classList.add('carousel')
+  leftBtn.classList.add('left-button')
+  rightBtn.classList.add('right-button')
+
+  img1.src = "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg"
+  img2.src = "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg"
+  img3.src = "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg"
+  img4.src = "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg"
+
+  carousel.appendChild(leftBtn)
+  carousel.appendChild(img1)
+  carousel.appendChild(img2)
+  carousel.appendChild(img3)
+  carousel.appendChild(img4)
+  carousel.appendChild(rightBtn)
+
+  return carousel
+}
+
+
+carouselCont.appendChild(CarouselMaker())
