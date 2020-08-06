896b3e635fbb2aaa0697d5af09079223c8b7b704
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-08-06 16:20:33 -0400

    finished Cards

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,61 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+import axios from 'axios'
+
+const cardsCont = document.querySelector('.cards-container')
+
+const CardMaker = data => {
+  const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+        const imgCont = document.createElement('div')
+            const img = document.createElement('img')
+        const byAuthor = document.createElement('span')
+
+  card.appendChild(headline)
+  card.appendChild(author)
+  author.appendChild(imgCont)
+  imgCont.appendChild(img)
+  author.appendChild(byAuthor)
+
+  card.classList.add('card')
+  headline.classList.add('headline')
+  author.classList.add('author')
+  imgCont.classList.add('img-container')
+  img.src = data.authorPhoto
+
+  headline.textContent = data.headline
+  byAuthor.textContent = data.authorName
+
+  headline.addEventListener('click', e => {
+    console.log(e.target.textContent)
+  })
+
+  return card
+}
+
+axios
+  .get(`https://lambda-times-api.herokuapp.com/articles`)
+  .then(res => {
+    // console.log(res.data.articles)
+    let superArr = [
+        ...res.data.articles.javascript, 
+        ...res.data.articles.node, 
+        ...res.data.articles.jquery, 
+        ...res.data.articles.bootstrap, 
+        ...res.data.articles.technology 
+    ]
+
+    // console.log(superArr)
+
+    superArr.forEach(item => {
+        // console.log(item)
+        cardsCont.appendChild(CardMaker(item))
+    })
+    
+  })
+  .catch(err => {
+    console.log(err)
+  })
